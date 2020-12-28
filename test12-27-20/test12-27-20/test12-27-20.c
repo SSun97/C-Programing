e45c5f936e0fca64c36139398f99e704e4fd528a
mingyu sun <sunmingyu.97@gmail.com>
2020-12-27 22:45:45 -0600

    update.c

--- test12-27-20/test12-27-20/test12-27-20.c
+++ test12-27-20/test12-27-20/test12-27-20.c
@@ -1,14 +1,119 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 
-int main()
-{
+int main(){
 
-	int a = 10;
-	int b = a--;
-	printf("a = %d b = %d\n", a, b);
+	int n=1
 
-	return 0;
+
 
 
 
 }
+
+
+
+
+
+
+
+
+//	int a = 1;
+//	while (a <= 100)
+//	{
+//		printf("%d\n", a);
+//
+//		a = a + 2;
+//	}
+//	
+//
+//		
+//
+//
+//
+//int main(){
+//
+//
+//
+//	//int a = 2221;
+//
+//	//if (a % 2 == 1)
+//	//	printf("this is odd number.\n");
+//	//else
+//	//{
+//	//	printf("this is not odd number.\n");
+//	//}
+//
+//	return 0;
+//
+//
+//
+//}
+//
+
+
+
+
+////#include <stdio.h>
+
+
+
+
+
+
+////
+////int main()
+////{
+////
+////	int a = 10;
+////	int b = a--;
+////	printf("a = %d b = %d\n", a, b);
+////
+////	return 0;
+////
+////
+////
+////}
+//
+//
+//
+////
+////#include <stdio.h>
+////
+////int main()
+////{
+////	int a = 10;
+//////	printf("%p\n", &a);
+////	int* p = &a; //p 是指针变量
+////	*p = 20;//解引用 *是操作符
+////	printf("a = %d\n", a);
+////	return 0;
+////
+////
+////}
+//
+//#include <stdio.h>
+//
+//struct Book
+//{
+//	char name[20];
+//	short price;
+//};
+//
+//int main(){
+//	struct Book b1 = {"CProgramming",55 };
+//
+//	struct Book* pb = &b1;
+//	
+//	printf("%s\n", pb->name);
+//	printf("%d\n", pb->price);
+//	//. 结构体变量。成员
+//	//-> 结构体指针 -> 成员
+//	//printf("Name:%s\n", b1.name);
+//	//printf("Price:%d\n", b1.price);
+//
+//	//b1.price = 15;
+//	//printf("changedPrice:%d\n", b1.price);
+//
+//	return 0;
+//
+//}
